74df6aac981aef65573ec59120a1498b19336311
malagenia <kubakry@fkfamily.com>
2022-09-09 13:12:29 +0200

    prawie skonfigurowany projekt (bez apachecommons)

--- src/main/java/pl/coderslab/Main01.java
+++ src/main/java/pl/coderslab/Main01.java
@@ -2,7 +2,11 @@ package pl.coderslab;
 
 public class Main01 {
     public static void main (String[] args) {
-        String a = reverseString ();
+
+    }
+    public static void showOptionList(){
+        String optionList[] = {"Please select an option", "add","remove","list","exit"};
+        System.out.println.ConsoleColors.BLUE (optionList[0]);
     }
 
 }
