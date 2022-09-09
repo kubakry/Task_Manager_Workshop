471089a6a74710f9e63d1caac6fbfa929980f77a
malagenia <kubakry@fkfamily.com>
2022-09-09 18:00:41 +0200

    dwie metody gotowe

--- src/main/java/pl/coderslab/Main01.java
+++ src/main/java/pl/coderslab/Main01.java
@@ -1,26 +1,18 @@
 package pl.coderslab;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main01 {
     public static void main (String[] args) {
-        showOptionList ();
+        inputOption ("tasks.csv");
         inputTasksFromFile ("tasks.csv");
-        Scanner scan = new Scanner(System.in);
-        String input = scan.next();
-        switch (input) {
-            case "add": addTask();break;
-            case "remove": removeTask();break;
-            case "list": listTask("tasks.csv");break;
-            case "exit": exitTask();break;
-// other options
-            default:
-                System.out.println("Please select a correct option.");
 
-        }
     }
 
     public static void showOptionList () {
@@ -48,19 +40,82 @@ public class Main01 {
         }
         return outputTasksArray;
     }
-    public static void addTask(){
 
+    public static void addTask (String fileName) {
+        Scanner scan1 = new Scanner (System.in);
+        System.out.println ("\u001B[37m" + "please put task description");
+        String taskDescription = scan1.next ();
+        System.out.println ("\033[0;33m" + taskDescription);
+        Scanner scan2 = new Scanner (System.in);
+        System.out.println ("\u001B[37m" + "please put task due date");
+        String taskDueDate = scan2.next ();
+        System.out.println ("\033[0;33m" + taskDueDate);
+        Scanner scan3 = new Scanner (System.in);
+        System.out.println ("\u001B[37m" + "is your task important? (y/n)");
+        String taskimportance = scan3.next ();
+        System.out.println ("\033[0;33m" + taskimportance);
+
+        switch (taskimportance) {
+            case "y":
+                taskimportance = "true";
+                break;
+            case "n":
+                taskimportance = "false";
+                break;
+        }
+        String newTaskLine = taskDescription + ", " + taskDueDate + ", " + taskimportance;
+        System.out.println ("\033[0;33m" + "whole line after concatening: " + newTaskLine + "\u001B[37m");
+        try {
+            Writer output;
+            output = new BufferedWriter (new FileWriter (fileName, true));  //clears file every time
+            output.append (newTaskLine);
+            output.close ();
+        } catch (IOException ex) {
+            System.out.println ("Błąd zapisu do pliku.");
+        }
+        listTask (fileName);
+    }
+
+    public static void removeTask (String fileName) {
 
     }
-    public static void removeTask(){
 
-    }
-    public static void listTask(String fileName){
+    public static void listTask (String fileName) {
         for (int i = 0; i < inputTasksFromFile (fileName).length; i++) {
-            System.out.println (inputTasksFromFile (fileName)[i]);
+            int n = i + 1;
+            System.out.println ("task nr " + n + ": " + inputTasksFromFile (fileName)[i]);
+        }
+        inputOption (fileName);
+
+    }
+
+    public static void exitTask (String fileName) {
+        System.out.println ("\033[0;31m" + "bye bye");
+        System.exit (0);
+    }
+
+    public static void inputOption (String fileName) {
+        showOptionList ();
+        Scanner scan = new Scanner (System.in);
+        String input = scan.next ();
+        switch (input) {
+            case "add":
+                addTask (fileName);
+                break;
+            case "remove":
+                removeTask (fileName);
+                break;
+            case "list":
+                listTask (fileName);
+                break;
+            case "exit":
+                exitTask (fileName);
+                break;
+// other options
+            default:
+                System.out.println ("Please select a correct option.");
+
         }
     }
-    public static void exitTask(){
 
-    }
 }
